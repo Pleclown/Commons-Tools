705c35b1cd6e4ee7f22394db8eaf52de5c7a3a7c
Pleclown <pleclown+github@gmail.com>
2014-01-10 17:48:41 +0100

    Update category.php

--- utils/category.php
+++ utils/category.php
@@ -2,6 +2,8 @@
 
 class category{
 
+const QUERY_CAT_BY_NAME ='select cat_pages, cat_subcats, cat_files from category where cat_title = ?;';
+
 const QUERY_USER_IN_CAT ='select distinct page_title from page, image where page_namespace = 6 and img_name = page_title and img_user = ? and page_id in (SELECT distinct cl_from from categorylinks where cl_to IN (?))';
 
 const QUERY_USER_NOT_IN_CAT ='select distinct page_title from page, image where page_namespace = 6 and img_name = page_title and img_user = ? and page_id not in (SELECT distinct cl_from from categorylinks where cl_to IN (?))';
@@ -10,7 +12,12 @@ const QUERY_UPLOADERS_IN_CAT = 'select distinct img_user_text, count(img_name) a
 
 const QUERY_FILES_IN_CAT_BY_MONTH = 'select DATE_FORMAT(img_timestamp,"%Y-%m") as created_month, count(img_name) as compte from image ,page where img_name = page_title and page_id in (SELECT distinct cl_from from categorylinks where cl_to IN (?)) group by created_month order by created_month;';
 
+  public $loaded= false;
   public $catname;
+  public $catfiles;
+  public $catpages;
+  public $catsubcats;
+  public $uploaders;
   private $connection;
    
   function __construct($aConnection, $aName)
@@ -24,9 +31,52 @@ const QUERY_FILES_IN_CAT_BY_MONTH = 'select DATE_FORMAT(img_timestamp,"%Y-%m") a
        $this->catname = $aName;
        $result = $this->connection->execute(user::QUERY_CAT_BY_NAME,array($this->catname));
        if ($result != NULL){
+         $this->catfiles = $result[0]['cat_files'];
+         $this->catpages = $result[0]['cat_pages'];
+         $this->catsubcats = $result[0]['cat_subcats'];
          $this->loaded = true;
        }
    } 
 
+   public function printCat()
+   {
+     
+      if ($this->loaded) {
+?>
+<fieldset><legend>Cat</legend>
+<p><strong>Name    : </strong> <?php echo $this->catname; ?></p>
+<p><strong>Subcats : </strong> <?php echo $this->catsubcats; ?></p>
+<p><strong>Pages   : </strong> <?php echo $this->catpages; ?></p>
+<p><strong>Files   : </strong> <?php echo $this->catfiles; ?></p>
+</fieldset>
+<?php
+     } else {
+       echo '<fieldset><legend>Cat</legend>
+Cat NOT found !
+</fieldset>';
+     }
+   }
 
+   public function uploadersInCat
+   {
+      $this->connection->execute(QUERY_UPLOADERS_IN_CAT,array($this->catname));
+
+      if ($result != NULL){
+        foreach ($result as $row)
+        {
+          $this->uploaders[$row['img_user_text']]= $row['compte'];
+        }
+
+      }
+   }
+
+?>
+<fieldset><legend>List</legend>
+<?php
+echo $list;
+?>
+</fieldset>
+<?php
+}     
+   }
 ?>
