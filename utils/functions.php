c82e34c0713e54ea3c420db83a06f8a6705dfffc
Pleclown <pleclown+github@gmail.com>
2014-01-10 13:54:28 +0100

    Update functions.php

--- utils/functions.php
+++ utils/functions.php
@@ -1,12 +1,4 @@
 <?php
-const QUERY_USER_IN_CAT ='select distinct page_title from page, image where page_namespace = 6 and img_name = page_title and img_user = ? and page_id in (SELECT distinct cl_from from categorylinks where cl_to IN (?))';
-
-const QUERY_USER_NOT_IN_CAT ='select distinct page_title from page, image where page_namespace = 6 and img_name = page_title and img_user = ? and page_id not in (SELECT distinct cl_from from categorylinks where cl_to IN (?))';
-
-const QUERY_UPLOADERS_IN_CAT = 'select distinct img_user_text, count(img_name) as compte from image, page, categorylinks where page_namespace = 6 and img_name = page_title and page_id = cl_from and cl_to in (?) group by img_user';
-
-const QUERY_FILES_IN_CAT_BY_MONTH = 'select DATE_FORMAT(img_timestamp,"%Y-%m") as created_month, count(img_name) as compte from image ,page where img_name = page_title and page_id in (SELECT distinct cl_from from categorylinks where cl_to IN (?)) group by created_month order by created_month;';
-
 function octets($n){
 	$i=0;
 	while ($n > 1024){
