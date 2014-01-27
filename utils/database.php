816d92098c214baf1d0d7af7431fb5f20ba8efb4
Pleclown <pleclown+github@gmail.com>
2014-01-27 16:16:17 +0100

    Update database.php

--- utils/database.php
+++ utils/database.php
@@ -21,5 +21,34 @@ public $connected = false;
     return $stmt->fetchAll();
   }
 
+  public function insert($query,$params,$returnid = false)
+  {
+    $result = -1;
+    $stmt = $this->connection->prepare($query);  
+    try {
+        $this->connection->beginTransaction();
+        $stmt->execute($params);
+        if $returnid = true 
+          $result = $this->connection->lastInsertId();
+        $this->connection->commit();
+    } catch(PDOException $e) {
+        $this->connection->rollback();
+        echo 'Error!: ' . $e->getMessage() . '</br>';
+    }
+    return $result;
+  }
+  
+  public function update($query,$params)
+  {
+    $stmt = $this->connection->prepare($query);  
+    try {
+        $this->connection->beginTransaction();
+        $stmt->execute($params);
+        $this->connection->commit();
+    } catch(PDOException $e) {
+        $this->connection->rollback();
+        echo 'Error!: ' . $e->getMessage() . '</br>';
+    }   
+  }
 }
 ?>
