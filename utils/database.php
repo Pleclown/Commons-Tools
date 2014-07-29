2e9b5be15b94977fd95204025191210b9ee93f49
Pleclown <pleclown+github@gmail.com>
2014-07-29 21:37:27 +0200

    Update database.php

--- utils/database.php
+++ utils/database.php
@@ -6,7 +6,7 @@ public $connected = false;
     //$ts_pw = posix_getpwuid(posix_getuid());
     $ts_mycnf = parse_ini_file("../replica.my.cnf");
     try {
-      $this->connection = new PDO('mysql:host=commonswiki.labsdb;dbname=commonswiki_p', $ts_mycnf['user'], $ts_mycnf['password']);
+      $this->connection = new PDO('mysql:host='.$db.'.labsdb;dbname='.$db.'_p', $ts_mycnf['user'], $ts_mycnf['password']);
       $this->connected = true;
     
     } catch (PDOException $e) {
@@ -28,7 +28,7 @@ public $connected = false;
     try {
         $this->connection->beginTransaction();
         $stmt->execute($params);
-        if $returnid = true 
+        if ($returnid = true)
           $result = $this->connection->lastInsertId();
         $this->connection->commit();
     } catch(PDOException $e) {
