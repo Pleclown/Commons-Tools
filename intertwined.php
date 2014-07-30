35fbe4046b60f0cc846e3e0caeaa9150744a0828
Pleclown <pleclown+github@gmail.com>
2014-07-30 21:42:02 +0200

    Update intertwined.php

--- intertwined.php
+++ intertwined.php
@@ -10,25 +10,28 @@ $h1='Intertwined contributions';
 include('utils/header.php');
 
 if (!empty($_GET)) {
+$project = $_GET['project'];
 $user1=$_GET['user1'];
 $user2=$_GET['user2'];
 }else{
+$project = '';
 $user1='';
 $user2='';
 }
 
 $metadb = new metadatabase;
-if ($metadabase->loaded)
-{
- echo 'OK';
-}
 ?>
 <fieldset><legend>Description</legend>
 This tool diplays the last 1000 cumulated contributions of the two users. If you want to see all the pages edited by both users, see <a href='//tools.wmflabs.org/intersect-contribs/' title='Intersect contribs'>Intersect contribs</a> by Pietrodn.
 </fieldset>
 <fieldset><legend>Users</legend>
 <form method="get" action="intertwined.php" id="mw-sulinfo-form1">
-<table border="0" id="mw-movepage-table"> 
+<table border="0" id="mw-movepage-table">
+<tr><td class='mw-label'><label for="wiki">Project </label></td><td class='mw-input'><select class="form-control" name="project" id="wikiDb" required>
+					<?php
+						$metadb->listSelectWiki($project);
+					?>
+					</select></td></tr>
 <tr><td class='mw-label'><label for="user1">User 1 :</label></td><td class='mw-input'><input id="user1" name="user1" type="text" value="<?php echo $user1; ?>"/></td>
 <tr><td class='mw-label'><label for="user2">User 2 :</label></td><td class='mw-input'><input id="user2" name="user2" type="text" value="<?php echo $user2; ?>"/></td>
 <tr><td>&#160;</td><td class='mw-submit'><input type="submit" value="Go !" /></td></tr>
