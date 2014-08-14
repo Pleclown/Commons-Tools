bb229fe5d8f52887186f4f1609d85a1e633d2d76
Pleclown <pleclown+github@gmail.com>
2014-08-14 23:06:47 +0200

    Update intertwined.php

--- intertwined.php
+++ intertwined.php
@@ -21,8 +21,8 @@ $user2='';
 
 $metadb = new metadatabase;
 ?>
-<fieldset><legend>Description</legend>
-This tool diplays the last 1000 cumulated contributions of the two users. If you want to see all the pages edited by both users, see <a href='//tools.wmflabs.org/intersect-contribs/' title='Intersect contribs'>Intersect contribs</a> by Pietrodn.
+<fieldset><legend>Intertwined contributions</legend>
+This tool displays the last 1000 cumulated contributions of the two users. If you want to see all the pages edited by both users, see <a href='//tools.wmflabs.org/intersect-contribs/' title='Intersect contribs'>Intersect contribs</a> by Pietrodn.
 </fieldset>
 <fieldset><legend>Users</legend>
 <form method="get" action="intertwined.php" id="mw-sulinfo-form1">
