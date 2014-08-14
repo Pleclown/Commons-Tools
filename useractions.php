68a8fe5082ef67db990de7b4cdb33369a6235cc0
Pleclown <pleclown+github@gmail.com>
2014-08-14 23:08:57 +0200

    Update useractions.php

--- useractions.php
+++ useractions.php
@@ -9,21 +9,29 @@ include('utils/header.php');
 
 
 if (!empty($_GET)) {
+$project = $_GET['project'];
         $name=$_GET['user'];
 }else{
         $name='';
+$project = '';
 }
+$metadb = new metadatabase;
 ?>
 <fieldset><legend>Uploadcounter</legend>
 <form method="get" action="useractions.php" id="mw-sulinfo-form1">
 <table border="0" id="mw-movepage-table"> 
+<tr><td class='mw-label'><label for="wiki">Project </label></td><td class='mw-input'><select class="form-control" name="project" id="wikiDb" required>
+					<?php
+					       echo $metadb->listSelectWiki($project);
+					?>
+					</select></td></tr>
 <tr><td class='mw-label'><label for="username">Username :</label></td><td class='mw-input'><input id="user" name="user" type="text" value="<?php echo $name; ?>"/></td><tr><td>&#160;</td><td class='mw-submit'><input type="submit" value="Go !" /></td></tr>
 </table>
 </form></fieldset>
 <?php
 if ($name != '') {
 $db = new database;
-if ($db->connect('commonswiki')) {
+if ($db->connect($project)) {
 $user = new user($db,$name);
 
 $user->printUser();
