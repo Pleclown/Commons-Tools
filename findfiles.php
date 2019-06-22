3c666099f2c5bbb36f8b3241617a57bbaed5d443
Pleclown <pleclown+github@gmail.com>
2019-06-22 12:50:55 +0200

    Update findfiles.php

--- findfiles.php
+++ findfiles.php
@@ -43,7 +43,7 @@ $user->printUser();
 $cat = new category($db,$category);
 $cat->printCat();
 
-$cat->getFilesInCatFor($user->user_id, $reverse);
+$cat->getFilesInCatFor($user->user_name, $reverse);
 $cat->printFilesInCatFor();
 
 }
