52ad1d2c4e536bfe3ac169129c6d74ac1a907fb0
Pleclown <pleclown+github@gmail.com>
2019-06-22 12:10:05 +0200

    Update category.php

--- utils/category.php
+++ utils/category.php
@@ -8,7 +8,7 @@ const QUERY_USER_IN_CAT ='select distinct page_title from page, image where page
 
 const QUERY_USER_NOT_IN_CAT ='select distinct page_title from page, image where page_namespace = 6 and img_name = page_title and img_user = ? and page_id not in (SELECT distinct cl_from from categorylinks where cl_to IN (?))';
 
-const QUERY_UPLOADERS_IN_CAT = 'select distinct img_user_text, count(img_name) as compte from image, page, categorylinks where page_namespace = 6 and img_name = page_title and page_id = cl_from and cl_to in (?) group by img_user order by count(img_name) desc';
+const QUERY_UPLOADERS_IN_CAT = 'select distinct actor_name, count(img_name) as compte from image, page, categorylinks, actor where page_namespace = 6 and img_name = page_title and page_id = cl_from and cl_to in (?) and actor_id=img_actor group by img_actor order by count(img_name) desc';
 
 const QUERY_FILES_IN_CAT_BY_MONTH = 'select DATE_FORMAT(img_timestamp,"%Y-%m") as created_month, count(img_name) as compte from image ,page where img_name = page_title and page_id in (SELECT distinct cl_from from categorylinks where cl_to IN (?)) group by created_month order by created_month;';
 
@@ -71,7 +71,7 @@ Cat NOT found !
       $this->uploadersCounter = 0;
       foreach ($result as $row)
       {
-        $this->uploaders[$row['img_user_text']]= $row['compte'];
+        $this->uploaders[$row['actor_name']]= $row['compte'];
         $this->uploadersCounter++;
       }
     }
