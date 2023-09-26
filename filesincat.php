828cf19338970ea24e9ba02b11fa6bdaa0850bf5
Pleclown <pleclown+github@gmail.com>
2023-09-26 10:37:52 +0200

    Change on noHTML

--- filesincat.php
+++ filesincat.php
@@ -9,10 +9,8 @@ $h1='Commons files in cat';
 include('utils/header.php');
 
 if (!empty($_GET)) {
-$name= noHTML($_GET['user']);
-$category= noHTML($_GET['category']);
+$category= $_GET['category'];
 }else{
-$name='';
 $category = '';
 }
 
@@ -22,7 +20,7 @@ $category = '';
 <p>Get information on files for a category, including count of members and time of uploads.
 <form method="get" action="filesincat.php" id="mw-sulinfo-form1">
 <table border="0" id="mw-movepage-table"> 
-<tr><td class='mw-label'><label for="category">Category :</label></td><td class='mw-input'><input id="category" name="category" type="text" value="<?php echo $category; ?>"/></td>
+<tr><td class='mw-label'><label for="category">Category :</label></td><td class='mw-input'><input id="category" name="category" type="text" value="<?php echo noHTML($category); ?>"/></td>
 <tr><td>&#160;</td><td class='mw-submit'><input type="submit" value="Go !" /></td></tr>
 </table>
 </form></fieldset>
